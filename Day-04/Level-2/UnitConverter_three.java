b7f643208739387bb9142c32139f63d7929ff41a
Nancy Mehra <mehranancy975@gmail.com>
2025-01-13 12:15:33 +0530

    Level-2 Files completed

---
+++ Day-04/Level-2/UnitConverter_three.java
@@ -0,0 +1,55 @@
+public class UnitConverter_three {
+
+    // Method to convert Fahrenheit to Celsius
+    public static double convertFahrenheitToCelsius(double fahrenheit) {
+        return (fahrenheit - 32) * 5 / 9;
+    }
+
+    // Method to convert Celsius to Fahrenheit
+    public static double convertCelsiusToFahrenheit(double celsius) {
+        return (celsius * 9 / 5) + 32;
+    }
+
+    // Method to convert pounds to kilograms
+    public static double convertPoundsToKilograms(double pounds) {
+        double pounds2kilograms = 0.453592;
+        return pounds * pounds2kilograms;
+    }
+
+    // Method to convert kilograms to pounds
+    public static double convertKilogramsToPounds(double kilograms) {
+        double kilograms2pounds = 2.20462;
+        return kilograms * kilograms2pounds;
+    }
+
+    // Method to convert gallons to liters
+    public static double convertGallonsToLiters(double gallons) {
+        double gallons2liters = 3.78541;
+        return gallons * gallons2liters;
+    }
+
+    // Method to convert liters to gallons
+    public static double convertLitersToGallons(double liters) {
+        double liters2gallons = 0.264172;
+        return liters * liters2gallons;
+    }
+
+    // Main method to demonstrate functionality
+    public static void main(String[] args) {
+        
+        double fahrenheit = 98.6;
+        double celsius = 37.0;
+        double pounds = 150.0;
+        double kilograms = 68.0;
+        double gallons = 5.0;
+        double liters = 18.93;
+
+        // Perform conversions and display results
+        System.out.println(fahrenheit + "°F = " + convertFahrenheitToCelsius(fahrenheit) + "°C");
+        System.out.println(celsius + "°C = " + convertCelsiusToFahrenheit(celsius) + "°F");
+        System.out.println(pounds + " pounds = " + convertPoundsToKilograms(pounds) + " kilograms");
+        System.out.println(kilograms + " kilograms = " + convertKilogramsToPounds(kilograms) + " pounds");
+        System.out.println(gallons + " gallons = " + convertGallonsToLiters(gallons) + " liters");
+        System.out.println(liters + " liters = " + convertLitersToGallons(liters) + " gallons");
+    }
+}
